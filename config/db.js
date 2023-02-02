5da1e836ae3824ed58d6603789435dbf07f698da
Karina Fernandez <karina.eliz.fernandez@gmail.com>
2023-02-02 08:54:34 -0300

    modificacion sequelize

--- config/db.js
+++ config/db.js
@@ -7,8 +7,8 @@ const op = Sequelize.Op;
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    port: '3306',
-    dialect: 'mysql',
+    port: "3306",
+    dialect: "mysql",
     define: {
         timestamps: false
     },
